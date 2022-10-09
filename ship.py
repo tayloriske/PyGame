1b35fe6ccb9dc5f648be78954e932bdcc5ee7991
Taylor Iske <taylori04@yahoo.com>
2022-10-09 11:18:37 -0500

    Adjusting Ship's Speed

--- ship.py
+++ ship.py
@@ -3,21 +3,26 @@ import pygame
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x)
+
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x += 1
-            
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
