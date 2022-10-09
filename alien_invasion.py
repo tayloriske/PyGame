9c4ee759006be5b84d8f1f457fa30dd74a5f6491
Taylor Iske <taylori04@yahoo.com>
2022-10-09 10:31:29 -0500

    Setting Background Color

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,19 +3,23 @@ import sys
 import pygame
 
 class AlienInvasion:
-    def__init__(self):
-    pygame.init()
+    def __init__(self):
+        pygame.init()
 
-    self.screen = pygame.display.set_mode((1200,800))
-    pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
 
-def run_game(self):
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        self.bg_color = (230, 230, 230)
 
-        pygame.display.flip()
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+        
+            self.screen.fill(self.bg_color)
+
+            pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
