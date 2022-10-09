5e615dae42571f5bb4907f0092421b1170f500a0
Taylor Iske <taylori04@yahoo.com>
2022-10-09 13:59:15 -0500

    Refactoring _update_bullets()

--- alien_invasion.py
+++ alien_invasion.py
@@ -67,12 +67,17 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                  self.bullets.remove(bullet)
+
+        self.check_bullet_alien_collisions()
+    
+    def _check_bullet_alien_collisions(self):
+
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
-            
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
