149555b5f31069c1a31d8305321ecd5e2ba9e873
Taylor Iske <taylori04@yahoo.com>
2022-10-09 10:49:42 -0500

    Drawing Ship to Screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -13,6 +14,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
@@ -22,6 +25,7 @@ class AlienInvasion:
                     sys.exit()
         
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -1,11 +1,11 @@
 import pygame
 
 class Ship:
-    def __init__(self):
+    def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
