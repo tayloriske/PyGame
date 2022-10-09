bc42fc47d55aa6531a28ff06a42fba17182987a4
Taylor Iske <taylori04@yahoo.com>
2022-10-09 14:51:33 -0500

    Resetting the Score

--- alien_invasion.py
+++ alien_invasion.py
@@ -176,6 +176,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
             self.aliens.empty()
             self.bullets.empty()
             self._create_fleet()
