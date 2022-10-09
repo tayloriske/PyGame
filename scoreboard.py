3ec96c7dac84437d2ea9d51e1241c63023fec4d4
Taylor Iske <taylori04@yahoo.com>
2022-10-09 15:00:07 -0500

    High Scores

--- scoreboard.py
+++ scoreboard.py
@@ -12,6 +12,8 @@ class Scoreboard:
 
         self.prep_score()
 
+        self.prep_high_score()
+
     def prep_score(self):
         rounded_score = round(self.stats.score, -1)
         score_str = "{:}".format(rounded_score)
@@ -23,3 +25,18 @@ class Scoreboard:
 
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def prep_high_score(self):
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+ 
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
