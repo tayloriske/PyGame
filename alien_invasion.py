e4066080ca4834289432f8109adaa15203ca30ab
Taylor Iske <taylori04@yahoo.com>
2022-10-09 14:50:43 -0500

    Updating Score as Aliens are shot down

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,6 +88,9 @@ class AlienInvasion:
 
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
@@ -101,7 +104,7 @@ class AlienInvasion:
         self.aliens.draw(self.screen)
 
         self.sb.show_score()
-        
+
         if not self.stats.game_active:
             self.play_button.draw_button()
 
--- settings.py
+++ settings.py
@@ -22,6 +22,7 @@ class Settings:
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
         self.fleet_direction = 1
+        self.alien_points = 50
 
     def increase_speed(self):
         self.ship_speed *= self.speedup_scale
