e9fb020443b810daa1e707cbab1d3e8b7df341de
Taylor Iske <taylori04@yahoo.com>
2022-10-09 11:15:43 -0500

    Moving Both Left and Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -41,10 +41,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                    
 if __name__ == "__main__":
     ai = AlienInvasion()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -10,11 +10,14 @@ class Ship:
         self.rect.midbottom = self.screen_rect.midbottom
 
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         if self.moving_right:
             self.rect.x += 1
-
+        if self.moving_left:
+            self.rect.x += 1
+            
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
