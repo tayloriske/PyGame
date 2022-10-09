cedb6869c6da5d7a79aab255e1deee595ea048cc
Taylor Iske <taylori04@yahoo.com>
2022-10-09 13:20:44 -0500

    Creating Row of Aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -77,7 +77,15 @@ class AlienInvasion:
     
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2* alien_width)
+
+        for alien_number in range(number_aliens_x):
+            alien = Alien(self)
+            alien.x = alien_width + 2* alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
