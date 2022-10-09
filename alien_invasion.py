cff17ba63b642630942b579bc6265412c222fbf2
Taylor Iske <taylori04@yahoo.com>
2022-10-09 11:45:26 -0500

    Deleting Old Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,11 +27,18 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
             self._update_screen()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                    
     def _fire_bullet(self):
         new_bullet = Bullet(self)
         self.bullets.add(new_bullet)
