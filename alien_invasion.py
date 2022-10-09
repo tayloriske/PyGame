e75124914d7cf0d99eb030dad453aaee4a6c3521
Taylor Iske <taylori04@yahoo.com>
2022-10-09 13:23:13 -0500

    Refactoring _create_fleett()

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,10 +82,15 @@ class AlienInvasion:
         number_aliens_x = available_space_x // (2* alien_width)
 
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2* alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self.create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
